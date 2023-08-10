d64b161595d4a3d2368c1698f1a3652596941d2f
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2023-08-11 01:42:54 +0600

    optimizer navbar and hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Hero() {
   return (
-    <header className=" font-black py-52 flex justify-center text-3xl md:text-[64px] text-[#AAFF30]">
-      <h1 className="w-[976px] font-black text-center h-[204px] uppercase leading-[105.5%] railway">
+    <header className=" font-black px-3 py-52 flex justify-center text-3xl sm:text-4xl md:text-5xl lg:text-[64px] text-[#AAFF30]">
+      <h1 className="w-[976px] font-black text-center h-[204px] uppercase leading-[105.5%]">
         <span className="text-white">Elevate</span> Your Business with Expert
         <span className="text-[#FF5252]"> Programming</span> <span className="text-white">Solutions</span>
         
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -11,23 +11,23 @@ function Navbar() {
 
   return (
     <header>
-      <nav className="bg-[#23232B] text-[#DEDEDE] flex justify-between items-center px-7 xl:justify-evenly py-7">
+      <nav className="bg-[#23232B] text-[#DEDEDE] flex justify-between items-center px-7 md:justify-evenly py-7">
         <div className="text-4xl w-32 h-14 font-black">
           <Image src="/logo.png" alt="Logo" width={123} height={54} />
         </div>
-        <div className="hidden xl:flex gap-7 text-2xl">
+        <div className="hidden md:flex xl:gap-7 gap-3 lg:text-xl xl:text-2xl">
             <Link href="./" className="cursor-pointer">Home</Link>
             <Link href="./" className="cursor-pointer">Services</Link>
             <Link href="./" className="cursor-pointer">About Us</Link>
             <Link href="./" className="cursor-pointer">Contact</Link>
             <Link href="./" className="cursor-pointer">Projects</Link>
         </div>
-        <div className="hidden xl:flex">
-          <button className="bg-[#AAFF30] w-48 h-11 rounded-3xl text-[#23232B] text-2xl font-black">
+        <div className="hidden md:flex">
+          <button className="bg-[#AAFF30] w-40 xl:w-48 lg:w-44 h-11 rounded-3xl text-[#23232B] lg:text-xl xl:text-2xl font-black">
             Get Started
           </button>
         </div>
-        <div className="xl:hidden flex" id="menu">
+        <div className="md:hidden flex" id="menu">
           <Image
             src="/menu.svg"
             alt="Menu"
