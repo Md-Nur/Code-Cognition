cac216cff5ea84972053a855f368755dbed62d48
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2023-08-11 21:36:00 +0600

    more efficient footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,21 +4,23 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="text-white text-center pt-10">
-      <div className="bg-[#16A072] flex flex-wrap gap-5 justify-evenly px-5 py-44">
+      <div className="bg-[#16A072] flex flex-wrap gap-10 justify-evenly px-5 py-44">
         <div className="w-[500px]">
           <p className="text-xl font-semibold">
-            <span className="text-[#AAFF30] text-5xl">Code</span>{" "}
-            <span className="text-3xl">&</span>
-            <br /> <span className="text-4xl">Cognition</span>
+            <span className="text-[#AAFF30] text-4xl lg:text-5xl">Code</span>{" "}
+            <span className="text-2xl lg:text-3xl">&</span>
+            <br /> <span className="text-3xl lg:text-4xl">Cognition</span>
           </p>
-          <p className="text-xl">
+          <p className="lg:text-xl">
             Is an ed-tech platform that trains kids and teenagers in programming
             and other tech skills
           </p>
         </div>
         <div className="">
-          <p className="text-2xl font-semibold pb-5 text-left">Quick Links</p>
-          <ul className="list-none text-left text-xl leading-10">
+          <p className="text:xl lg:text-2xl font-semibold pb-5 lg:text-left">
+            Quick Links
+          </p>
+          <ul className="list-none lg:text-left text-lg lg:text-xl leading-10">
             <li>About StackJunior</li>
             <li>Courses</li>
             <li>Career</li>
@@ -30,8 +32,8 @@ function Footer() {
         </div>
         <div>
           <div className="">
-            <p className="text-2xl font-semibold pb-5 text-left">Socials</p>
-            <ul className="list-none text-left flex gap-5">
+            <p className="text-xl lg:text-2xl font-semibold pb-5 lg:text-left">Socials</p>
+            <ul className="list-none lg:text-left flex gap-5">
               <li>
                 <Image src={"/social/facebook.svg"} height={40} width={40} />
               </li>
@@ -47,17 +49,21 @@ function Footer() {
             </ul>
           </div>
           <div className="">
-            <p className="text-xl text-left mt-5 leading-10">Contact Us</p>
-            <ul className="text-left text-xl leading-10">
-                <li>01521758709</li>
-                <li>01770171779</li>
+            <p className="text-lg lg:text-xl lg:text-left mt-5 leading-10">Contact Us</p>
+            <ul className="lg:text-left text-xl leading-10">
+              <li>
+                <a href="tel: 01521758709">0121758709</a>
+              </li>
+              <li>
+                <a href="tel: 01770171779">01770171779</a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
       <div className="bg-[#066A4C] p-9">
-        <p className="text-xl font-semibold">Code & Cognition</p>
-        <p className="">© 2021 Code & Cognition. All rights reserved.</p>
+        <p className="text-lg lg:text-xl font-semibold">Code & Cognition</p>
+        <p className="">© 2023 Code & Cognition. All rights reserved.</p>
       </div>
     </footer>
   );
